4b5300f78b55587ef1e32ca132b8580fd7182b89
poonam-kumawat <mailpoonam2002@gmail.com>
2023-01-14 21:58:07 +0530

    some changes

---
+++ todo/src/app/service/basicauthentication.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http:HttpClient) { }
+  authenticate(username: any,password: any){
+    if(username==="poonam" && password==='123'){
+      sessionStorage.setItem('authenticaterUser',username)
+    return true;
+    }
+    return false;
+  }
+
+  executeAuthenticationService(username: any,password: any){
+
+    let basicAuthHeaderString='Basic ' + Window.bind(username + ':' +password);
+    
+    let headers=new HttpHeaders(
+      {
+        Authorization:basicAuthHeaderString
+      }
+    )
+    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`,
+    {headers});
+    
+  }
+ 
+
+  isUserLoggedIn(){
+    let user=sessionStorage.getItem('authenticaterUser')
+    return !(user===null)
+  }
+  logout(){
+    sessionStorage.removeItem('authenticaterUser')
+
+  }
+
+}
+export class AuthenticationBean{
+  constructor(public message:string){}
+}
+
